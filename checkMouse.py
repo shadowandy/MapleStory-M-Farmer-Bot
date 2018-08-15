1a69db91e42f52e643fe4747e465e4d4674a631c
Andy Lim <shadowandy.sg@gmail.com>
2018-08-15 09:05:00 +0800

    Mouse coordinates checking
    updates

---
+++ checkMouse.py
@@ -0,0 +1,12 @@
+#! python
+import pyautogui, sys
+print('Press Ctrl-C to quit.')
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print positionStr,
+        print '\b' * (len(positionStr) + 2),
+        sys.stdout.flush()
+except KeyboardInterrupt:
+    print '\n'
