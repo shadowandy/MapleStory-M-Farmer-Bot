4be5cf933fdc5f6b37e0d88ea8399f4e56e0bbe9
Andy Lim <shadowandy@shadowandys-iMac.local>
2018-08-27 10:20:16 +0800

    New Quest Bot
    Refactored some of the codes

--- mapleQuestBot.py
+++ mapleQuestBot.py
@@ -2,53 +2,54 @@ from pyautogui import locateCenterOnScreen, locateOnScreen, click, mouseDown, mo
 import random
 import threading
 import time
+import sys
+import datetime
 
-#screenDimension = [2560, 1440]
-#screenScalingFactor = 1
-screenDimension = [2880, 1800]
-screenScalingFactor = 2
+screenDimension = [2560, 1440]
+screenScalingFactor = 1
+#screenDimension = [2880, 1800]
+#screenScalingFactor = 2
 
-status = ['1/4 Starting Quest', 'Skipping Narratives', '2/4 Accepting Quest', '3/4 Completing Quest', '4/4 Claiming Rewards', '2/4 Accepting Quest', '3/4 Completing Quest']
-images = ['questScroll.png', 'questSkip.png', 'questAccept.png', 'questComplete.png', 'questClaim.png', 'questAvailable.png', 'questMultiComplete.png']
-
-regionSizeInit = [screenDimension[0]/2, screenDimension[1]/2]
-regionSizeScaled = [240, 50]
-
-regionSearchX = [0, 0, screenDimension[0]/2, screenDimension[0]/2, screenDimension[0]/4, 0, 0]
-regionSearchY = [0, 0, screenDimension[1]/2, screenDimension[1]/2, screenDimension[1]/2, screenDimension[1]/2, screenDimension[1]/2]
-regionSearchW = [regionSizeInit[0]-1, regionSizeInit[0]-1, regionSizeInit[0]-1, regionSizeInit[0]-1, regionSizeInit[0]-1, regionSizeInit[0]-1, regionSizeInit[0]-1]
-regionSearchH = [regionSizeInit[1]-1, regionSizeInit[1]-1, regionSizeInit[1]-1, regionSizeInit[1]-1, regionSizeInit[1]-1, regionSizeInit[1]-1, regionSizeInit[1]-1]
+questStart = [480, 540]
+imagesToggle = 'questClaim.png'
 
 started = 0
+images = ['questScroll.png', 'questClaim.png', 'questComplete.png', 'questSkip.png', 'questAccept.png', 'questComplete.png', 'questAvailable.png', 'questMultiComplete.png']
+searchX = dict(shadow='andy')
+searchY = dict(shadow='andy')
+searchM = [240, 240]
 
 def questBot():
-	global started, regionSearchX, regionSearchY, regionSearchH, regionSearchW
+	global started, searchX, searchY
 	if started:
-		coord = [0, 0]
+		coord = [0,0]
 		for x in range(1,len(images)):
-			if locateCenterOnScreen(images[x], grayscale=True, region=(regionSearchX[x], regionSearchY[x], regionSearchW[x], regionSearchH[x])) is not None:
-				time.sleep(2)
-				(coord[0], coord[1]) = locateCenterOnScreen(images[x], grayscale=True, region=(regionSearchX[x], regionSearchY[x], regionSearchW[x], regionSearchH[x]))
-				mouseClick(coord[0]/screenScalingFactor, coord[1]/screenScalingFactor)
-				print("x=" + str(coord[0]) + " y=" + str(coord[1]))
-				if regionSearchW[x] != regionSizeScaled[0]:
-					regionSearchX[x] = coord[0] - regionSizeScaled[0]/2*screenScalingFactor
-					regionSearchY[x] = coord[1] - regionSizeScaled[1]/2*screenScalingFactor
-					regionSearchW[x] = regionSizeScaled[0]*screenScalingFactor
-					regionSearchH[x] = regionSizeScaled[1]*screenScalingFactor
-				print(status[x])
-
-				if x == 4:
-					started = not started
-				if x == 2 or x == 5:
-					time.sleep(random.uniform(25,52))
+			found = 0
+			try:
+				if images[x] in searchX:
+					if locateCenterOnScreen(images[x], grayscale=True, region=(int(searchX[images[x]]),int(searchY[images[x]]),searchM[0],searchM[1])) is not None:
+						(coord[0], coord[1]) = locateCenterOnScreen(images[x], grayscale=True, region=(int(searchX[images[x]]),int(searchY[images[x]]),searchM[0],searchM[1]))
+						found = not found
+				else:
+					if locateCenterOnScreen(images[x], grayscale=True) is not None:
+						(coord[0], coord[1]) = locateCenterOnScreen(images[x], grayscale=True)
+						searchX[images[x]] = str(coord[0] - searchM[0]/2)
+						searchY[images[x]] = str(coord[1] - searchM[1]/2)
+						found = not found
+				if found:
+					mouseClick(coord[0]/screenScalingFactor, coord[1]/screenScalingFactor)
+					print("[" + datetime.datetime.fromtimestamp(time.time()).strftime('%d-%m %H:%M:%S') + "] - Clicked '" + images[x] + "'")
+					if images[x] == imagesToggle:
+							started = not started
+					found = not found
+			except IOError as e:
+				print("Catching IO Error from pyautogui")
 	else:
-		#mouseClick(480, 540)
-		mouseClick(240, 380)
-		print(status[0])
+		mouseClick(questStart[0], questStart[1])
+		print("[" + datetime.datetime.fromtimestamp(time.time()).strftime('%d-%m %H:%M:%S') + "] - Clicked 'Start Quest'")
 		started = not started
 
 def mouseClick(xi,yi):
 	mouseDown(x=xi, y=yi)
-	time.sleep(0.5)
+	time.sleep(random.uniform(0.4,0.7))
 	mouseUp()
